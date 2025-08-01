3199493cb7ea53998e3e1a15eb4a1ac6fb5e5119
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2025-08-01 20:47:15 +0530

    Added features section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
 import StepsSection from "../components/StepsSection";
+import { BentoGrid } from "../ui/BentoGrid";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <BentoGrid />
     </div>
   );
 };
