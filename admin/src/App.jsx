716b2b1f52ad92d6d385952f169e0df06b315e99
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2025-08-02 05:31:38 +0530

    Added Admin Frontend

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import MainLayout from "../../client/src/Layouts/MainLayout";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import AdminLogin from "./components/Login";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
-    <>
-      <div>Hi</div>
-    </>
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<AdminLogin />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </MainLayout>
   );
 }
 
